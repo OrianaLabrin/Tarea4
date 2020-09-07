694f3f9ce3914556c45a2ddb639bff598ec31951
OrianaLabrin <orianalabrinz@gmail.com>
2020-09-07 09:09:19 -0300

    Listo para ejecutar

--- operaciones.py
+++ operaciones.py
@@ -1,3 +1,14 @@
-
-#Suma
+x=2
+y=3.4
+z=4.0
+Print (f'Sabemos que x={x}, y={y}, z={z}')
+#suma 
+print (f'Suma: x+y=(x+y) por lo tanto {x}+{y}={x+y}')
 #Resta
+print (f'Resta: {x}-{y}={x-y}'
+#Multiplicacion 
+print (f'Multiplicacion: {x}*{y}={x*y}')
+#division 
+print (f'division: {x}/{y}={x/y}')
+#Resto
+print (f'El resto de {x} dividido por {y} es {x%y}')
