333ad071ae9760b48d49f52b55f1f60a4e8b7550
OrianaLabrin <orianalabrinz@gmail.com>
2020-09-07 10:01:50 -0300

    Correccion

--- diccionario.py
+++ diccionario.py
@@ -3,11 +3,11 @@ Alumno1 = {'nombre': 'Esteban Quito'}
 Alumno2 = {'nombre': 'Juanito Alimaña'}
 Alumno3 = {'nombre': 'Arturo Prat'}
 Alumno4 = {'nombre': 'Lola Palooza'}
-Notas_programacion
+#Notas_programacion
 np = {'A1':[2,5,4,6,5], 'A2':[3,5,6,5,4], 'A3':[7,5,3,5,6], 'A4': [6,3,4,5,5]}
-Notas_fisica
+#Notas_fisica
 nf = {'A1':[4,6,5,4,4], 'A2':[7,3,6,6,4], 'A3':[3,6,6,4,4], 'A4': [4,5,7,7,3]}
 
-print (f' Nombre:', Alumno1['nombre'])
-print (f' Notas_programacion:', np['A1'])
-print (f' Notas_fisica': nf[A1])
+print (f'Nombre: {Alumno1["nombre"]}')
+print (f'Notas_programacion: {np["A1"]}')
+print (f'Notas_fisica: {nf["A1"]}')
--- operaciones.py
+++ operaciones.py
@@ -1,14 +1,14 @@
 x=2
 y=3.4
 z=4.0
-Print (f'Sabemos que x={x}, y={y}, z={z}')
+print (f'Sabemos que x={x}, y={y}, z={z}')
 #suma 
-print (f'Suma: x+y=(x+y) por lo tanto {x}+{y}={x+y}')
+print (f'Suma: x+y es igual a {x+y}')
 #Resta
-print (f'Resta: {x}-{y}={x-y}'
+print (f'Resta: x-y es igual a {x-y}')
 #Multiplicacion 
-print (f'Multiplicacion: {x}*{y}={x*y}')
+print (f'Multiplicacion: x*y es igual a {x*y}')
 #division 
-print (f'division: {x}/{y}={x/y}')
+print (f'division: x/y es igual a {x/y}')
 #Resto
 print (f'El resto de {x} dividido por {y} es {x%y}')
